09db3095e9b1aa9d69d8e23f9c90a862062d7e1e
Timothy Gu <timothygu99@gmail.com>
2015-02-17 18:55:37 -0500

    Better test output

--- test/index.js
+++ test/index.js
@@ -5,11 +5,24 @@ var fs = require('fs');
 
 var transform = require('../');
 
+var failed = false;
+
+function assertEqual(output, expected) {
+  console.log('\tOutput:  ', JSON.stringify(output));
+  console.log('\tExpected:', JSON.stringify(expected));
+  if (output !== expected) {
+    console.log('\tFAILED');
+    failed = true;
+  } else {
+    console.log('\tPASSED');
+  }
+}
+
 var input = fs.readFileSync(__dirname + '/input.md', 'utf8');
 var expected = fs.readFileSync(__dirname + '/expected.html', 'utf8');
 
 var output = transform.render(input);
 fs.writeFileSync(__dirname + '/output.html', output);
-assert(output === expected, 'output.html should equal expected.html');
+assertEqual(output, expected);
 
 console.log('test passed');
