be9edb357e5ec2ae7cf99723018d5decbe154ccf
Timothy Gu <timothygu99@gmail.com>
2015-02-17 18:53:57 -0500

    Mark output format as XML

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var MarkdownIt = require('markdown-it');
 
 exports.name = 'markdown-it';
-exports.outputFormat = 'html';
+exports.outputFormat = 'xml';
 exports.render = function (str, options) {
   var md = MarkdownIt(options);
   return md.render(str);
