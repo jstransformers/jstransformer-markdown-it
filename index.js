77be63209cbee35091391bebcf0ad418886b94da
Timothy Gu <timothygu99@gmail.com>
2015-03-29 10:20:41 -0700

    Use `'html'` as output format
    Per jstransformers/jstransformer#3.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var MarkdownIt = require('markdown-it');
 
 exports.name = 'markdown-it';
-exports.outputFormat = 'xml';
+exports.outputFormat = 'html';
 exports.render = function (str, options) {
   var md = MarkdownIt(options);
   return md.render(str);
