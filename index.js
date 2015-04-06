1702969833ea6f827ccc01371cb3af1018318bee
Rob Loach <robloach@gmail.com>
2015-04-06 13:55:58 -0400

    feat(inputFormats): Add inputFormats

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var MarkdownIt = require('markdown-it');
 
 exports.name = 'markdown-it';
 exports.outputFormat = 'html';
+exports.inputFormats = ['markdown-it', 'markdown', 'md'];
 exports.render = function (str, options) {
   var md = MarkdownIt(options);
   return md.render(str);
